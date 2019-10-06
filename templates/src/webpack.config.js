c4abf279d2e0588e69c6e4ed9b73563a3ce73f48
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-10-06 13:26:32 +0900

    add webpack.config.js

---
+++ templates/src/webpack.config.js
@@ -0,0 +1,15 @@
+modules.exports = {
+    entry: './src/index.js',
+    output: { 
+        filename: 'index.js',
+        path: `${__dirname}`
+    },
+    module: {
+        rules: [
+            {
+                test: /\.svelte$/,
+                use: 'svelte-loader'
+            }
+        ]
+    }
+}
