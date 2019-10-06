d8349ebe728d6c42f63773f419f1395fe0f0567f
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-10-06 13:44:35 +0900

    replace navbar

--- templates/src/src/App.svelte
+++ templates/src/src/App.svelte
@@ -2,4 +2,11 @@
     let str = "Hello, Flask & Svelte!"
 </script>
 
+<nav class="navbar navbar-light bg-light">
+    <a class="navbar-brand" href="#">
+        <img src="https://getbootstrap.jp/docs/4.3/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
+        Flask Examples
+    </a>
+</nav>
+
 <h1 class="display-1">{str}</h1>
